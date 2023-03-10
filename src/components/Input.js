7e639231cd350a0309cf10c161015095f99b65c8
sanghakyi <sang105kr@gmail.com>
2023-03-10 11:04:57 +0900

    20230310-2

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Children } from 'react';
 import { Dimensions } from 'react-native';
 import styled from 'styled-components/native';
+import Protypes from 'prop-types';
 
 const StyledInput = styled.TextInput.attrs(({ theme }) => ({
   placeholderTextColor: theme.main,
@@ -15,7 +16,7 @@ const StyledInput = styled.TextInput.attrs(({ theme }) => ({
   color: ${({ theme }) => theme.text};
 `;
 
-const Input = ({ placeholder, onChangeText }) => {
+const Input = ({ placeholder, value, onChangeText, onSubmitEditing }) => {
   const { width } = Dimensions.get('window');
   return (
     <StyledInput
@@ -27,9 +28,19 @@ const Input = ({ placeholder, onChangeText }) => {
       returnKeyType="done"
       // secureTextEntry={true} 입력문자 보호
       // multiline={true}  여러줄입력
+      value={value}
       onChangeText={onChangeText}
+      onSubmitEditing={onSubmitEditing}
     />
   );
+};
+
+// props속성의 타입과 필수여부체크
+Input.prototypes = {
+  placeholder: Protypes.string,
+  value: Protypes.string.isRequired,
+  onChangeText: Protypes.func.isRequired,
+  onSubmitEditing: Protypes.func.isRequired,
 };
 
 export default Input;
